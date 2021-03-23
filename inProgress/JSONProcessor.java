1a817505c55234993bbbb8dcffd97f4f165feba7
Kristina Gu <kristina@astralclocktowerstudios.com>
2021-03-23 12:00:35 -0400

    Initial commit for JSON parsing capabilities

---
+++ inProgress/JSONProcessor.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONProcessor {
+    
+}
