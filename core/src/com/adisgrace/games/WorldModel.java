3baccb948a3dc1d0bfdbf9d36f1373d58706f18c
Brian Zhu <bkz3@cornell.edu>
2021-03-28 15:19:19 -0400

    returns successful hack message on interact hack

--- core/src/com/adisgrace/games/WorldModel.java
+++ core/src/com/adisgrace/games/WorldModel.java
@@ -204,7 +204,7 @@ public class WorldModel {
 			return this.scan(targetname, fact);
 		else
 			this.hack(targetname, fact);
-		return "";
+		    return "You successfully hack the node.";
 	}
 
 	/**
