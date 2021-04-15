a8c74e5222c60be5c183ceb49d0f66517a80d85b
Faris Aziz <fsa22@cornell.edu>
2021-04-15 10:14:18 -0400

    Changed all node images based on hacking and scanning

--- core/src/com/adisgrace/games/GameController.java
+++ core/src/com/adisgrace/games/GameController.java
@@ -902,7 +902,6 @@ public class GameController implements Screen {
                     switch (world.interactionType(nodeInfo[0], nodeInfo[1])) {
                         case HACK:
                             if(world.getPlayer().canHack()) {
-
                                 Texture node = new Texture("node/N_UnscannedNode_1.png");
                                 TextureRegion[][] regions = new TextureRegion(node).split(
                                         node.getWidth() / 10,
@@ -916,14 +915,10 @@ public class GameController implements Screen {
 
                                 Texture combined = GameCanvas.combineTextures(tRegion, node_regions[0][0]);
 
-
-
-                                //tRegion = new TextureRegion(new Texture(Gdx.files.internal("targetmale/green.png")));
-                                drawable = new TextureRegionDrawable(new TextureRegion(combined));
+                                TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(combined));
 
                                 button.setStyle(new ImageButton.ImageButtonStyle(null, null, null,
-                                        new TextureRegionDrawable(new TextureRegion(new Texture(
-                                                Gdx.files.internal("node/red.png")))), null, null));
+                                        drawable, null, null));
                                 createDialogBox(world.interact(nodeInfo[0], nodeInfo[1]));
                                 reloadDisplayedNodes();
                             } else {
@@ -932,9 +927,21 @@ public class GameController implements Screen {
                             break;
                         case SCAN:
                             if(world.getPlayer().canScan()) {
+                                Texture node = new Texture("node/N_ScannedNode_2.png");
+                                TextureRegion[][] regions = new TextureRegion(node).split(
+                                        node.getWidth() / 10,
+                                        node.getHeight() / 6);
+                                TextureRegion tRegion = regions[0][0];
+
+                                Texture node_base = new Texture("node/N_NodeBase_1.png");
+                                TextureRegion[][] node_regions = new TextureRegion(node_base).split(
+                                        node_base.getWidth() / 6,
+                                        node_base.getHeight() / 2);
+
+                                Texture combined = GameCanvas.combineTextures(tRegion, node_regions[0][0]);
+                                TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(combined));
                                 button.setStyle(new ImageButton.ImageButtonStyle(null, null, null,
-                                        new TextureRegionDrawable(new TextureRegion(new Texture(
-                                                Gdx.files.internal("node/blue.png")))), null, null));
+                                        drawable, null, null));
                                 createDialogBox(world.interact(nodeInfo[0], nodeInfo[1]));
                                 reloadDisplayedNodes();
                             } else {
