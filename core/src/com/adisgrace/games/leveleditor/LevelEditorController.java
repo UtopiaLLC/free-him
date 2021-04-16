f4751c974c5c5223ad0d6eae37ff6f2235a27683
Kristina Gu <kristina@astralclocktowerstudios.com>
2021-04-15 21:59:24 -0400

    Remove print statements

--- core/src/com/adisgrace/games/leveleditor/LevelEditorController.java
+++ core/src/com/adisgrace/games/leveleditor/LevelEditorController.java
@@ -595,7 +595,6 @@ public class LevelEditorController implements Screen {
                 button.addListener(new ChangeListener() {
                     @Override
                     public void changed(ChangeEvent event, Actor actor) {
-                        System.out.println("Saved");
                         saveLevel();
                     }
                 });
