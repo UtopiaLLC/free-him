e60ab002f579e785960b3b10d4521c30f2363746
Brian Zhu <bkz3@cornell.edu>
2021-04-23 20:39:28 -0400

    ButtonFactory but actually this time

--- core/src/com/adisgrace/games/util/ButtonFactory.java
+++ core/src/com/adisgrace/games/util/ButtonFactory.java
@@ -1,34 +1,64 @@
 package com.adisgrace.games.util;
 
+import com.adisgrace.games.InputController;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class ButtonFactory {
+
     /**
-     *
-     * @param up
-     * @param down
-     * @param checked
-     * @param hoverText
-     * @param stage
+     * Creates and returns an ImageButton with the linked assets.
+     * This does NOT attach a listener; that needs to be done elsewhere with button.addListener.
+     * @param upAssetPath (Relative or absolute) path to asset for unclicked button
+     * @param downAssetPath (Relative or absolute) path to asset for button when clicking
+     * @param checkedAssetPath (Relative or absolute) path to asset for button when selected
      * @return
      */
-    public static ImageButton makeInteractToolbarButton(
-            Stage stage,
-            TextureRegionDrawable up, TextureRegionDrawable down, TextureRegionDrawable checked,
-            String hoverText, Skin skin
-    ){
-        ImageButton button = new ImageButton(up, down, checked);
+    public static ImageButton makeImageButton(String upAssetPath, String downAssetPath, String checkedAssetPath){
+        ImageButton button = new ImageButton(
+                new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(upAssetPath)))),
+                new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(downAssetPath)))),
+                new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(checkedAssetPath))))
+        );
         button.setTransform(true);
         button.setScale(1);
+        return button;
+    }
 
-        Label hoverLabel = new Label(hoverText, skin);
+    /**
+     * Creates and returns an ImageButton with the linked assets and adds a
+     * ClickListener using the passed functions.
+     * @param upAssetPath (Relative or absolute) path to asset for unclicked button
+     * @param downAssetPath (Relative or absolute) path to asset for button when clicking
+     * @param checkedAssetPath (Relative or absolute) path to asset for button when selected
+     * @param onClick Method to run when clicked
+     * @return
+     */
+    public static ImageButton makeImageButton(String upAssetPath, String downAssetPath, String checkedAssetPath,
+                                              Runnable onClick){
+        ImageButton button = makeImageButton(upAssetPath, downAssetPath, checkedAssetPath);
+        button.addListener(InputController.getInstance().getButtonListener(onClick));
+        return button;
+    }
 
-
+    /**
+     * Creates and returns an ImageButton with the linked assets and adds a
+     * ClickListener using the passed functions.
+     * @param upAssetPath (Relative or absolute) path to asset for unclicked button
+     * @param downAssetPath (Relative or absolute) path to asset for button when clicking
+     * @param checkedAssetPath (Relative or absolute) path to asset for button when selected
+     * @param onClick Method to run when clicked
+     * @param onEnter Method to run when mouse enters button
+     * @param onExit Method to run when mouse leaves button
+     * @return
+     */
+    public static ImageButton makeImageButton(String upAssetPath, String downAssetPath, String checkedAssetPath,
+                                              Runnable onClick, Runnable onEnter, Runnable onExit){
+        ImageButton button = makeImageButton(upAssetPath, downAssetPath, checkedAssetPath);
+        button.addListener(InputController.getInstance().getButtonListener(onClick, onEnter, onExit));
         return button;
     }
 }
