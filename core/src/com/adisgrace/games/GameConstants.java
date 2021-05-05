1fd27d8e0900f70de7a6cc5aa5ff0e84443ce686
Faris Aziz <fsa22@cornell.edu>
2021-05-05 19:10:29 -0400

    Started Game Constants file

---
+++ core/src/com/adisgrace/games/GameConstants.java
@@ -0,0 +1,12 @@
+package com.adisgrace.games;
+
+public class GameConstants {
+    /* Predefined stress levels for FactNodes */
+    public enum StressRating {
+        NONE, LOW, MED, HIGH
+    }
+
+    /* Constants for stress rating values */
+    public static final int SR_NONE = 0, SR_LOW = 5, SR_MED = 10, SR_HIGH = 20;
+    public static final int exampleConstant = 1; //Remove
+}
