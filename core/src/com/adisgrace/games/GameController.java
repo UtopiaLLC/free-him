6764acaece19742ebc154952551f0f015c9263d7
Brian Zhu <bkz3@cornell.edu>
2021-05-07 20:09:25 -0400

    sanity now goes down

--- core/src/com/adisgrace/games/GameController.java
+++ core/src/com/adisgrace/games/GameController.java
@@ -1165,7 +1165,7 @@ public class GameController implements Screen {
      */
     public void updateStats(){
 //        stressBar.setValue(levelController.getPlayerStress());
-        stressBar.setFillAmount(levelController.getPlayerStress()/PlayerModel.MAX_STRESS);
+        stressBar.setFillAmount(1-levelController.getPlayerStress()/PlayerModel.MAX_STRESS);
 //        stressBar.setFillAmount(1f);
         bitecoinAmount.setText(Integer.toString((int)levelController.getPlayerCurrency()));
 //        ap.setText("AP: " + Integer.toString(levelController.getAP()));
